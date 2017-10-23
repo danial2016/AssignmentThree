c9c7d6aadc710fc2d9eb6f22ab3a1a4b0b2672ba
naser94 <naser.kakadost@hotmail.com>
2017-10-23 16:51:30 +0200

    Jag heter Mardokh

--- app/src/main/java/com/example/daniel/assignmentthree/GalleryFragment.java
+++ app/src/main/java/com/example/daniel/assignmentthree/GalleryFragment.java
@@ -1,13 +1,22 @@
 package com.example.daniel.assignmentthree;
 
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.Paint;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.GridView;
+import android.widget.ImageView;
 
 public class GalleryFragment extends Fragment {
-
+    // Keep all Images in array
+   CapturePicFragment capture;
+    GridView gridView;
+    ImageView imageview;
     public GalleryFragment() {
         // Required empty public constructor
     }
@@ -16,6 +25,30 @@ public class GalleryFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_gallery2, container, false);
+      View view = inflater.inflate(R.layout.fragment_gallery2, container, false);
+        this.capture = new CapturePicFragment();
+        gridView = (GridView) view.findViewById(R.id.grid_view);
+
+        // Instance of ImageAdapter Class
+        gridView.setAdapter(new ImageAdapter(getActivity()));
+        imageview = (ImageView) view.findViewById(R.id.imageviewer);
+        listener();
+        return view;
+
+
     }
+
+    private void listener() {
+        imageview.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                 imageview.setImageBitmap(capture.retrieveImage());
+
+            }
+        });
+    }
+
+
 }
+
+
